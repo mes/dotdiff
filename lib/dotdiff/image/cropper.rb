10a485c93785c088b7e7fc2007b9dd3db5024fc4
Andrea <om1noa@gmail.com>
2017-12-08 10:23:05 +0100

    Update cropper.rb
    Use mini_magick instead of rmagick

--- lib/dotdiff/image/cropper.rb
+++ lib/dotdiff/image/cropper.rb
@@ -1,27 +1,32 @@
-require 'rmagick'
+require 'mini_magick'
+
+# Use mini_magick instead of rmagick (which requires ImageMagick6)
+# @see https://github.com/minimagick/minimagick#thinking-of-switching-from-rmagick
 
 module DotDiff
   module Image
     module Cropper
       def crop_and_resave(element)
         image = load_image(fullscreen_file)
-        image.crop!(
-          element.rectangle.x,
-          element.rectangle.y,
-          width(element, image),
-          height(element, image)
-        )
 
+        # @see http://www.imagemagick.org/script/command-line-options.php?#crop
+        crop_area =
+          '' + width(element, image).to_s +
+          'x' + height(element, image).to_s +
+          '+' + element.rectangle.x.to_s +
+          '+' + element.rectangle.y.to_s
+
+        image.crop crop_area
         image.write(cropped_file)
       end
 
       def load_image(file)
-        Magick::Image.read(file).first
+        MiniMagick::Image.open('tmp/capybara/' + file)
       end
 
       def height(element, image)
         element_height = element.rectangle.height + element.rectangle.y
-        image_height = image.rows
+        image_height = image.height
 
         if element_height > image_height
           image_height - element.rectangle.y
@@ -32,7 +37,7 @@ module DotDiff
 
       def width(element, image)
         element_width = element.rectangle.width + element.rectangle.x
-        image_width = image.columns
+        image_width = image.width
 
         if element_width > image_width
           image_width - element.rectangle.x
